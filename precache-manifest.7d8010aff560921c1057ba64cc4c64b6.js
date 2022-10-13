798ba3cd7a478d8c426e979dab297e5a64155e4a
salah_survivor <salaheddinenaji2000@gmail.com>
2022-10-13 22:01:27 +0100

    Updates

---
+++ precache-manifest.7d8010aff560921c1057ba64cc4c64b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49b7a8356e746c5d368",
+    "url": "/posting-app/static/css/main.2e068a55.chunk.css"
+  },
+  {
+    "revision": "c49b7a8356e746c5d368",
+    "url": "/posting-app/static/js/main.c4a46a00.chunk.js"
+  },
+  {
+    "revision": "82579ca6ca5709135b34",
+    "url": "/posting-app/static/js/runtime~main.c6d71900.js"
+  },
+  {
+    "revision": "3878696014878d7e8991",
+    "url": "/posting-app/static/css/2.a8de5da3.chunk.css"
+  },
+  {
+    "revision": "3878696014878d7e8991",
+    "url": "/posting-app/static/js/2.25367bec.chunk.js"
+  },
+  {
+    "revision": "e8353ab84e98a3f809b4",
+    "url": "/posting-app/static/js/3.25923653.chunk.js"
+  },
+  {
+    "revision": "daf59bea1467351067c79f6de2e022fe",
+    "url": "/posting-app/static/media/admin.daf59bea.jpg"
+  },
+  {
+    "revision": "ef3717f03731d9c5a51c4f96e75a54be",
+    "url": "/posting-app/index.html"
+  }
+];
